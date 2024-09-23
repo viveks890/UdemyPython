14a5083b80f435f3c038fee3aabfaad4dd2a3b46
Vivek Chander <viveksingh19948901166662@gmail.com>
2024-09-23 09:11:24 +0530

    Add FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,5 @@
+# Print out numbers from 1 to 100 (including 100).
+# But, instead of printing multiples of 3, print "Fizz"
+# Instead of printing multiples of 5, print "Buzz"
+# Instead of printing multiples of both 3 and 5, print "FizzBuzz".
+
